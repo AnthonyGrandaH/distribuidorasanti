51b095bfe7c2c9465166fcbbf5f795dc46bf7706
AnthonyGrandaH <apgrandah@utn.edu.ec>
2021-03-25 08:19:10 -0500

    Registro Egresos

---
+++ distribuidorasantiEJB/ejbModule/distribuidorasanti/model/registro/managers/ManagerEgresos.java
@@ -0,0 +1,90 @@
+package distribuidorasanti.model.registro.managers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+import distribuidorasanti.model.core.entities.InvProducto;
+import distribuidorasanti.model.core.entities.RegEgreso;
+import distribuidorasanti.model.core.entities.RegIngreso;
+import distribuidorasanti.model.core.entities.VenVenta;
+import distribuidorasanti.model.core.managers.ManagerDAO;
+import distribuidorasanti.model.registros.dto.RegistroEgresosDTO;
+import distribuidorasanti.model.registros.dto.RegistroIngresosDTO;
+
+/**
+ * Session Bean implementation class ManagerEgresos
+ */
+@Stateless
+@LocalBean
+public class ManagerEgresos {
+
+    /**
+     * Default constructor. 
+     */
+	@EJB
+	ManagerDAO mDAO;
+	double PrecioTotal;
+    public ManagerEgresos() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public List<RegIngreso> findAllEgresos(){
+    	return mDAO.findAll(InvProducto.class, "idIngresos");
+    }
+    public void insertarEgresos(RegEgreso nuevoEgreso) throws Exception{
+    	
+    	mDAO.insertar(nuevoEgreso);
+    }
+ 
+   public List<RegistroEgresosDTO> findAllRegistroEgresosDTO(){
+	   List<RegistroEgresosDTO> listaEgresosDTO=new ArrayList<RegistroEgresosDTO>();
+	   RegistroEgresosDTO registroEgresosDTO;
+	   List<InvProducto> listaProductos= mDAO.findAll(InvProducto.class);
+	   for(int i=0;i<listaProductos.size();i++) {
+
+		   registroEgresosDTO=new RegistroEgresosDTO(listaProductos.get(i).getCodProducto(), listaProductos.get(i).getProNombre(), 
+				   listaProductos.get(i).getProPresioAd(), listaProductos.get(i).getProFechaCompra());
+		   listaEgresosDTO.add(registroEgresosDTO);
+	   }
+	   return listaEgresosDTO;
+   }
+
+    
+   public List<RegistroEgresosDTO> findAllRegistroByFecha(List<RegistroEgresosDTO> listaEgresos,int mes, int anio){
+	   List<RegistroEgresosDTO> listanuevoEgresosDTO=new ArrayList<RegistroEgresosDTO>();
+	   RegistroEgresosDTO registroEgresosDTO;
+	   double precioTotalVentas=0;
+	   int mesRegistro;
+	   int anioRegistro;
+	   for(int i=0;i<listaEgresos.size();i++) {
+		   registroEgresosDTO=listaEgresos.get(i);
+		   mesRegistro=registroEgresosDTO.getProFechaCompra().getMonth() +1;
+		   System.out.println("Mes manager: " +mesRegistro);
+		   anioRegistro=registroEgresosDTO.getProFechaCompra().getYear()+1900;
+		   System.out.println("Año manager: " +anioRegistro);
+		   if(mesRegistro==mes && anioRegistro==anio) {
+			   listanuevoEgresosDTO.add(registroEgresosDTO);
+			   precioTotalVentas= precioTotalVentas+registroEgresosDTO.getProPrecioAd();
+		   }
+	   }
+	   PrecioTotal=precioTotalVentas;
+	   return listanuevoEgresosDTO;
+	   
+   }
+
+
+
+public double getPrecioTotal() {
+	return PrecioTotal;
+}
+
+
+
+public void setPrecioTotal(double precioTotal) {
+	PrecioTotal = precioTotal;
+}
+}
---
+++ distribuidorasantiWeb/src/distribuidorasanti/controller/registros/BeanRegEgresos.java
@@ -0,0 +1,133 @@
+package distribuidorasanti.controller.registros;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import distribuidorasanti.controller.JSFUtil;
+import distribuidorasanti.model.core.entities.InvProducto;
+import distribuidorasanti.model.core.entities.RegEgreso;
+import distribuidorasanti.model.core.entities.RegIngreso;
+import distribuidorasanti.model.core.entities.VenVenta;
+import distribuidorasanti.model.registro.managers.ManagerEgresos;
+import distribuidorasanti.model.registro.managers.ManagerRegIngreso;
+import distribuidorasanti.model.registros.dto.RegistroEgresosDTO;
+import distribuidorasanti.model.registros.dto.RegistroIngresosDTO;
+import distribuidorasanti.model.ventas.dtos.ProductoDto;
+
+@Named
+@SessionScoped
+public class BeanRegEgresos implements Serializable {
+	@EJB
+	ManagerEgresos mEgreso;
+	private List<RegistroEgresosDTO> listadoEgresos;
+	private List<RegistroEgresosDTO> listadoRespaldo;
+	private RegistroEgresosDTO nuevoEgreso;
+	private double total = 0;
+	private int anioEgreso;
+	private int mesEgreso;
+
+	public BeanRegEgresos() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@PostConstruct
+
+	public void inicializar() {
+		listadoEgresos = mEgreso.findAllRegistroEgresosDTO();
+		listadoRespaldo = listadoEgresos;
+	}
+
+	public String actionMenuEgresos() {
+		listadoEgresos = mEgreso.findAllRegistroEgresosDTO();
+		listadoRespaldo = listadoEgresos;
+		return "egresos";
+	}
+
+	public void actionListenerBuscarRegistroPorFecha() {
+		System.out.println("Mes Ingresado " + mesEgreso);
+		System.out.println("Año Ingresado" + anioEgreso);
+		if (anioEgreso == 0) {
+			listadoEgresos = listadoRespaldo;
+			total = 0;
+		} else {
+			listadoEgresos = listadoRespaldo;
+			listadoEgresos = mEgreso.findAllRegistroByFecha(listadoEgresos, mesEgreso, anioEgreso);
+			total = mEgreso.getPrecioTotal();
+			JSFUtil.crearMensajeINFO("Registro generado exitosamente");
+		}
+	}
+
+	public void actionListenerCrearRegistroEgreso() {
+		try {
+			if (total == 0) {
+				JSFUtil.crearMensajeERROR("No se puede Generar el Reporte: Total:0");
+			} else {
+				RegEgreso egresoNuevo = new RegEgreso();
+				egresoNuevo.setFechaConsulta("" + anioEgreso);
+				egresoNuevo.setTotalEgresos(total);
+				mEgreso.insertarEgresos(egresoNuevo);
+				listadoEgresos = mEgreso.findAllRegistroEgresosDTO();
+				listadoRespaldo = listadoEgresos;
+				JSFUtil.crearMensajeINFO("Registro Ingresado Existosamente");
+			}
+		} catch (Exception e) {
+			JSFUtil.crearMensajeERROR(e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+	public List<RegistroEgresosDTO> getListadoEgresos() {
+		return listadoEgresos;
+	}
+
+	public void setListadoEgresos(List<RegistroEgresosDTO> listadoEgresos) {
+		this.listadoEgresos = listadoEgresos;
+	}
+
+	public List<RegistroEgresosDTO> getListadoRespaldo() {
+		return listadoRespaldo;
+	}
+
+	public void setListadoRespaldo(List<RegistroEgresosDTO> listadoRespaldo) {
+		this.listadoRespaldo = listadoRespaldo;
+	}
+
+	public RegistroEgresosDTO getNuevoEgreso() {
+		return nuevoEgreso;
+	}
+
+	public void setNuevoEgreso(RegistroEgresosDTO nuevoEgreso) {
+		this.nuevoEgreso = nuevoEgreso;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public int getAnioEgreso() {
+		return anioEgreso;
+	}
+
+	public void setAnioEgreso(int anioEgreso) {
+		this.anioEgreso = anioEgreso;
+	}
+
+	public int getMesEgreso() {
+		return mesEgreso;
+	}
+
+	public void setMesEgreso(int mesEgreso) {
+		this.mesEgreso = mesEgreso;
+	}
+
+}
